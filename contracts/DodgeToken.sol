bb2353e535284ae29bef05f17b67b395f0db56e7
Chandrashekhar0001 <shekharupadhyay7983@gmail.com>
2024-02-11 00:05:12 +0530

    make tokens burnable

--- contracts/DodgeToken.sol
+++ contracts/DodgeToken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MyToken is ERC20Capped {
+contract MyToken is ERC20Capped,ERC20Burnable {
     address payable public owner;
     constructor(uint cap) ERC20("DodgeToken","DGT") ERC20Capped(cap * (10 ** decimals())){
         owner = payable(msg.sender);
